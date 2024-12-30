dc036b66675695008f9556b78024ca6b62454354
joshuamg41 <88290099+joshuamg41@users.noreply.github.com>
2024-12-30 12:44:44 -0400

    start commit

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,8 @@
+const ProfilePage = () => {
+  return (
+    <div className="flex flex-col gap-4 p-6 md:p-10">
+        <h2>Hola mundo</h2>
+    </div>
+  )
+}
+export default ProfilePage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+import type { Metadata } from "next"
+import { ThemeProvider } from "@/components/theme-provider"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -23,11 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
